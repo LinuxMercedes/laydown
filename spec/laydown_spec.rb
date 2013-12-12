c05ab86f2d1c8ac80056ddab23763c31262e59a5
LinuxMercedes <LinuxMercedes@gmail.com>
2013-12-12 01:39:18 -0600

    Adding tests for paragraphs and inline blocks

--- spec/laydown_spec.rb
+++ spec/laydown_spec.rb
@@ -11,4 +11,34 @@ describe LayDownParser do
 
 		expect(parser.parse(text)).to_not eq(nil)
 	end
+
+	it "parses a paragraph" do
+		parser = LayDownParser.new
+
+		text =<<'END'
+This is a paragraph.
+This is another line in that paragraph.
+Moooooore text.
+END
+
+		expect(parser.parse(text)).to_not eq(nil)
+	end
+
+	it "parses inline blocks" do
+		parser = LayDownParser.new
+
+		text =<<'END'
+This is a paragraph.
+This is a line `` with an empty inline block.
+This is another line in that paragraph.
+This is a line with a `filled inline block`.
+This line has an escaped \` that should parse fine.
+This line has an `escaped \` inside a ` block.
+Moooooore text.
+This line has a `tagged, inline` block.
+END
+
+		expect(parser.parse(text)).to_not eq(nil)
+	end
+
 end
